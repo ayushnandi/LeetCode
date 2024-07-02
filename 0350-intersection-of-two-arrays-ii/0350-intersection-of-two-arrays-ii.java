bf46657f680ba188d7b512fa32dc83ed791b57e8
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-07-02 22:59:56 +0530

    Time: 3 ms (90.95%), Space: 43.2 MB (49.41%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        ArrayList <Integer> arr= new ArrayList<>();
-        for(int i = 0 , j = 0 ; i < nums1.length && j < nums2.length ;){
-            if(nums1[i] == nums2[j]){
-                arr.add(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]>nums2[j]){
-                j++;
-            }
-            else{
-                i++;
-            }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i : nums1){
+            map.put(i, map.getOrDefault(i , 0)+1);
+        }
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0 ; i < nums2.length ; i++ ){
+           if(map.containsKey(nums2[i]) && map.get(nums2[i])>0){
+               map.put(nums2[i],map.get(nums2[i])-1);
+               arr.add(nums2[i]);
+           }
         }
         int [] a = new int [arr.size()];
         for(int i = 0 ; i < arr.size() ; i++ ){
