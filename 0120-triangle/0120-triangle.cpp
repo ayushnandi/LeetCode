c190646dbddc443406728644211c47e58b6f1169
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-07-03 16:02:04 +0530

    Time: 2 ms (87.77%), Space: 11 MB (82.93%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,32 +2,16 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        std::vector<std::vector<int>> dp(n, std::vector<int>(triangle[n-1].size(), 0));
+        std::vector<int> dp = triangle[n-1];  // Start with the last row
         
-        dp[0][0] = triangle[0][0];
-        
-        // Fill the first column of the dp array
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = triangle[i][0] + dp[i-1][0];
-        }
-        
-        // Fill the rest of the dp array
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < triangle[i].size(); j++) {
-                if (j < triangle[i].size() - 1) {
-                    dp[i][j] = triangle[i][j] + std::min(dp[i-1][j-1], dp[i-1][j]);
-                } else {
-                    dp[i][j] = triangle[i][j] + dp[i-1][j-1];
-                }
+        // Start from the second last row and move upwards
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                dp[j] = triangle[i][j] + std::min(dp[j], dp[j+1]);
             }
         }
         
-        // Find the minimum path sum in the last row
-        int minPathSum = INT_MAX;
-        for (int i = 0; i < triangle[n-1].size(); i++) {
-            minPathSum = std::min(minPathSum, dp[n-1][i]);
-        }
-        
-        return minPathSum;
+        // The top element now contains the minimum path sum
+        return dp[0];
     }
 };
