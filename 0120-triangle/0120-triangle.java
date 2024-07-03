d22deb405dad71a8026c38e5bc1125eb3477e82c
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-07-03 12:35:31 +0530

    Time: 5 ms (33.13%), Space: 44.3 MB (30.86%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,16 +1,12 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int [][] dp  = new int  [triangle.size()][triangle.get(triangle.size()-1).size()];
-        // for(int  i = 0 ; i< )
         int n = dp.length;
         int m = dp[0].length;
-        // System.out.println(n+" "+m);
-
         dp[0][0] = (triangle.get(0)).get(0);
-        // System.out.println(dp[0][0]);
+
         for(int i = 1 ; i < n ; i++ ){
             dp[i][0] = (triangle.get(i)).get(0) + dp[i-1][0];
-            // System.out.println(dp[i][0]);
         }
         for(int i = 1 ; i < dp.length ; i++ ){
             for(int  j = 1 ; j < (triangle.get(i)).size(); j++ ){
@@ -18,7 +14,6 @@ class Solution {
                     dp[i][j] = (triangle.get(i)).get(j)+Math.min(dp[i-1][j-1],dp[i-1][j]);
                 }
                 else{
-                    // System.out.println((triangle.get(i)).size());
                     dp[i][j] = (triangle.get(i)).get(j)+dp[i-1][j-1];
                 }
             }
