33b11c0c71944f0e3f5fad580307af0b5db9ebb6
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-07-07 18:19:17 +0530

    Time: 12 ms (100%), Space: 40.9 MB (5.05%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -2,14 +2,13 @@ class Solution {
     public int numWaterBottles(int b, int n) {
         int store = 0;
         int left = 0;
-        int f = 0;
-         while (b > 0) {
+        while(b>0){
             store += b;
-            int newBottles = (b + left) / n;
-            left = (b + left) % n;
-            b = newBottles;
+            int f = (b+left)/n;
+            left = (b+left)%n;
+            b = f;
+            System.out.println("Store = "+store+" left = "+left+" bottel = "+b);
         }
-        store += b;
         System.out.println(store);
         return store;
     }
