8576b66dba195e803c8f3da4da30eff1c11bfb3d
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-07-10 12:06:35 +0530

    Time: 1 ms (99.42%), Space: 42 MB (51.62%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,17 +1,15 @@
 class Solution {
     public int minOperations(String[] logs) {
-        int count = 0 ;
-        for(int i = 0 ; i < logs.length ; i++ ){
-            if(logs[i].charAt(0)=='.'&&logs[i].charAt(1)=='.'){
-                count--;
-            }
-            else if(logs[i].charAt(0)!='.'&& Character.isLetter(logs[i].charAt(0)) || Character.isDigit(logs[i].charAt(0)) ){
+        int count = 0;
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (count > 0) {
+                    count--;
+                }
+            } else if (!log.equals("./")) {
                 count++;
             }
-            if(count<0){
-                count = 0;
-            }
         }
-        return count> 0 ? count : 0;
+        return count;
     }
 }
