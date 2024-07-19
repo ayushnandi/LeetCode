be72851f106f5869c90fadfe79fd18eb5a6d555f
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-07-19 22:33:29 +0530

    Time: 2 ms (93.93%), Space: 45.2 MB (32.97%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -1,20 +1,25 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] arr) {
-        int max = 0;
-        int [] row = new int [arr[0].length];
-        int [] col = new int [arr.length];
+        int [] dpCol = new int [arr[0].length];
+        Arrays.fill(dpCol,-1);
         ArrayList<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>(); 
+        int min = 999999;
+        int index = 0;
         for(int i = 0 ; i < arr.length ; i++ ){
-           col[i] = Rmin(arr,i);
-           map.put(col[i],map.getOrDefault(col[i],0)+1);
-        }
-        for(int i = 0 ; i < arr[0].length ; i++ ){
-            row[i] = Vmax(arr,i);
-            if(map.containsKey(row[i])){
-                list.add(row[i]);
-                return list;
+            min = 999999;
+            for(int j = 0 ; j < arr[0].length; j++ ){
+                if(arr[i][j]<min){
+                    min = arr[i][j];
+                    index = j;
+                }
             }
+            if(dpCol[index]==-1){
+                dpCol[index]=Vmax(arr,index);
+            }
+            if(dpCol[index]==min){
+                list.add(min);
+                return list;
+            } 
         }
         return list;
     }
@@ -25,11 +30,42 @@ class Solution {
         }
         return max;
     }
-    public int Rmin(int [][] arr, int n){
-        int min = 999999;
-        for(int i = 0 ; i < arr[0].length; i++ ){
-            min = Math.min(min, arr[n][i]);
-        }
-        return min;
-    }
 }
+
+
+
+// class Solution {
+//     public List<Integer> luckyNumbers (int[][] arr) {
+//         int max = 0;
+//         int [] row = new int [arr[0].length];
+//         int [] col = new int [arr.length];
+//         ArrayList<Integer> list = new ArrayList<>();
+//         HashMap<Integer, Integer> map = new HashMap<>(); 
+//         for(int i = 0 ; i < arr.length ; i++ ){
+//            col[i] = Rmin(arr,i);
+//            map.put(col[i],map.getOrDefault(col[i],0)+1);
+//         }
+//         for(int i = 0 ; i < arr[0].length ; i++ ){
+//             row[i] = Vmax(arr,i);
+//             if(map.containsKey(row[i])){
+//                 list.add(row[i]);
+//                 return list;
+//             }
+//         }
+//         return list;
+//     }
+//     public int Vmax(int [][] arr, int n){
+//         int max = 0;
+//         for(int i = 0 ; i < arr.length; i++ ){
+//             max = Math.max(max, arr[i][n]);
+//         }
+//         return max;
+//     }
+//     public int Rmin(int [][] arr, int n){
+//         int min = 999999;
+//         for(int i = 0 ; i < arr[0].length; i++ ){
+//             min = Math.min(min, arr[n][i]);
+//         }
+//         return min;
+//     }
+// }
