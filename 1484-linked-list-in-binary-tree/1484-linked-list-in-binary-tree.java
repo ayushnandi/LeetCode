8b0d3bb83ead714c60253ddc2bd604d94e4cb7b8
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-09-07 16:40:54 +0530

    Time: 1 ms (97.88%), Space: 44.6 MB (96.12%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
@@ -1,45 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
-        if(root == null) return false;
-        if(head.val == root.val){
-            if(isnext(head, root)){
-                return true;
-            }
+        if (root == null) return false;
+        if (head.val == root.val && isNext(head, root)) {
+            return true;
         }
         return isSubPath(head, root.left) || isSubPath(head, root.right);
     }
-    public boolean isnext(ListNode head, TreeNode root){
-        if(root == null ) return false;
-        System.out.print(root.val+" ");
-        if(head.val == root.val && head.next==null) return true;
-        if(head.next==null) return false;
-        if(head.val!=root.val) return false;
-        System.out.print(root.val+" ");
-        return isnext(head.next,root.left) || isnext(head.next,root.right);
+
+    private boolean isNext(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null || head.val != root.val) return false;
+        return isNext(head.next, root.left) || isNext(head.next, root.right);
     }
 }
