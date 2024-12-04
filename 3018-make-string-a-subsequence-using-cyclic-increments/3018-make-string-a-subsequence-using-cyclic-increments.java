fcd837be3ab3bd7e7a2a5fffab53ace102ea0cdd
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-12-04 21:56:30 +0530

    Time: 6 ms (71.31%), Space: 46.1 MB (5.92%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean canMakeSubsequence(String str1, String str2) {
-        int j = 0;
-        int cnt = 0;
+        int j = 0; // Pointer for str2
         if (str1.length() < str2.length())
-            return false;
+            return false; // Impossible if str1 is shorter than str2
+
         for (int i = 0; i < str1.length(); i++) {
             if (j >= str2.length()) {
-                return true;
+                return true; // All characters of str2 matched
             }
-            if (str1.charAt(i) == str2.charAt(j) || (str1.charAt(i) + 1 - 'a') % 26 == str2.charAt(j) - 'a')
-                j++;
+            char ch1 = str1.charAt(i);
+            char ch2 = str2.charAt(j);
+            // Check direct match or match after cyclic increment
+            if (ch1 == ch2 || (ch1 + 1 - 'a') % 26 == ch2 - 'a') {
+                j++; // Move to the next character in str2
+            }
         }
-        if (j >= str2.length()) {
-            return true;
-        }
-        return false;
+        return j >= str2.length(); // True if all of str2 is matched
     }
 }
