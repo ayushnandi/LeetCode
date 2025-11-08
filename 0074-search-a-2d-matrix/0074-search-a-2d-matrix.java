c907ed0bc9d84351d89895cfc550ba5b1c849955
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2025-11-09 01:01:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,29 +1,6 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int t) {
-        if(matrix.length==1){
-            for(int j = 0 ; j < matrix[0].length;j++ ){
-                    if(matrix[0][j]==t){
-                        return true;
-                    }
-                }
-            }
-        else{
-            for(int i = 0 ; i < matrix.length-1 ; i++ ){
-                if(matrix[i+1][0]>t){
-                    for(int j = 0 ; j < matrix[0].length;j++ ){
-                        if(matrix[i][j]==t){
-                            return true;
-                        }
-                    }
-                    break;
-                }
-            }
-            for(int j = 0 ; j < matrix[0].length;j++ ){
-                    if(matrix[matrix.length-1][j]==t){
-                        return true;
-                    }
-                }
-        }
-        return false;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return find(target,matrix,0,0,target.length,traget[0].length);
     }
+    public 
 }
