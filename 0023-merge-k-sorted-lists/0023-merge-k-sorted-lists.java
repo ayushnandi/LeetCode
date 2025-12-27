cb53cf0bae6b2a406e437aaeb11fe6a964e47002
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2025-12-27 23:43:02 +0530

    Time: 8 ms (22.97%), Space: 44.5 MB (96.41%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,7 +9,33 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
+    public ListNode mergeKLists(ListNode[] arr) {
+        int k = arr.length;
+        if(k == 1){
+            return arr[0];
+        }
+        else if(k == 0){
+            return null;
+        }
         
+        // return arr[0];
+        ArrayList<Integer> array = new ArrayList<>();
+        for(int i = 0 ; i < k ; i++ ){
+            ListNode n1 = arr[i];
+            while(n1!=null){
+                array.add(n1.val);
+                n1 = n1.next;
+            }
+        }
+        Collections.sort(array);
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        
+        for (int val : array) {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+        
+        return dummy.next;
     }
 }
