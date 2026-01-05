7d2ad8c8f38f0540dd0a54ee1c0eabda618c115d
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2026-01-05 23:49:30 +0530

    Time: 1 ms (99.79%), Space: 43.8 MB (60.33%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -4,22 +4,17 @@ class Solution {
         int sign = 1;
         long result = 0;
 
-        // 1. Skip leading spaces
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
-
-        // 2. Check sign
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
             sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
 
-        // 3. Convert digits
         while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
             result = result * 10 + (s.charAt(i) - '0');
 
-            // 4. Handle overflow
             if (sign * result > Integer.MAX_VALUE)
                 return Integer.MAX_VALUE;
             if (sign * result < Integer.MIN_VALUE)
