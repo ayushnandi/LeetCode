223916297ea4e74ae24d5cbec3ac94ec734d0402
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2026-07-17 01:11:59 +0530

    Time: 45 ms (26.29%), Space: 47 MB (73.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,10 +4,8 @@ class Solution {
         int [] arr = new int [2];
         for(int i = 0 ; i < nums.length ; i++ ){
             for(int j = i+1 ; j< nums.length ; j ++){
-                if(nums[i]+nums[j]==t){
-                    arr[0]=i;
-                    arr[1]=j;
-                    return arr;
+                if(nums[i]+nums[j]==t ){
+                    return new int [] {i,j};
                 }
             }
         }
