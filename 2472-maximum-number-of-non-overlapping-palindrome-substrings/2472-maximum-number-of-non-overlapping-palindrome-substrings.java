4a7436fc83fa876b1677c608afdbd059eb7cf400
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2026-09-15 17:39:05 +0530

    Time: 150 ms (16.08%), Space: 62.2 MB (16.08%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,27 +1,27 @@
 class Solution {
     public int maxPalindromes(String s, int k) {
         int n = s.length();
-        if (k == 1)
-            return n;
-
-        int res = 0;
-
-        for (int i = 0; i <= n - k; i++) {
-            if (check(s, i, i + k - 1)) {
-                res++;
-                i += k - 1;
-            } else if (i < n - k && check(s, i, i + k)) {
-                res++;
-                i += k;
+        int[] dp = new int[n+1];
+        boolean[][] isPal = new boolean[n][n];
+        for(int len = 1 ; len <= n ; len++) {
+            for(int i = 0 ; i +len - 1 < n ; i++) {
+                int j = i+len-1;
+                if(len == 1) isPal[i][j] = true;
+                else if(len == 2) {
+                    isPal[i][j] = (s.charAt(i) == s.charAt(j));
+                }else {
+                    isPal[i][j] = (s.charAt(i)==s.charAt(j)) && isPal[i+1][j-1];
+                }
             }
         }
-        return res;
-    }
-
-    boolean check(String s, int l, int r) {
-        for (; l < r; l++, r--)
-            if (s.charAt(l) != s.charAt(r))
-                return false;
-        return true;
+      for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1]; 
+            for (int l = i - k; l >= 0; l--) {
+                if (isPal[l][i - 1]) {
+                    dp[i] = Math.max(dp[i], dp[l] + 1);
+                }
+            }
+        }
+        return dp[n];
     }
 }
